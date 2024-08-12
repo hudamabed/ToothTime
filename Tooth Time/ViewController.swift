d0b75ccad5e3245c9554134840c4a12b7c3d4bb0
Huda M. A'abed <hudamabed@gmail.com>
2024-08-12 18:29:57 +0300

    Initial Commit

---
+++ Tooth Time/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tooth Time
+//
+//  Created by Huda M. A'abed on 12/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
